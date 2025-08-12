eb23f1904f600c0e13f55d3b4027144fe9092199
sbsteacher <id@gmail.com>
2025-08-12 13:17:54 +0900

    FeedComment - post - 작업 중

--- src/main/java/com/green/greengram/application/feedcomment/FeedCommentController.java
+++ src/main/java/com/green/greengram/application/feedcomment/FeedCommentController.java
@@ -1,7 +1,14 @@
 package com.green.greengram.application.feedcomment;
 
+import com.green.greengram.application.feedcomment.model.FeedCommentPostReq;
+import com.green.greengram.config.model.ResultResponse;
+import com.green.greengram.config.model.UserPrincipal;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class FeedCommentController {
     private final FeedCommentService feedCommentService;
 
+    @PostMapping
+    public ResultResponse<?> postFeedComment(@AuthenticationPrincipal UserPrincipal userPrincipal
+                                           , @Valid @RequestBody FeedCommentPostReq req) {
+        log.info("signedUserId: {}", userPrincipal.getSignedUserId());
+        log.info("req: {}", req);
+        return null;
+    }
 }
