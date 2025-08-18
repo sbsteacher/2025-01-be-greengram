e4a91ef9909afce8093e64330e45929dce513059
sbsteacher <id@gmail.com>
2025-08-18 17:20:56 +0900

    likeCount 처리

--- src/main/java/com/green/greengram/application/feed/model/FeedGetRes.java
+++ src/main/java/com/green/greengram/application/feed/model/FeedGetRes.java
@@ -18,6 +18,7 @@ public class FeedGetRes {
     private String writerNickName;
     private String writerPic;
     private int isLike; //0: 좋아요 아닌 피드, 1: 좋아요한 피드
+    private int likeCount; //좋아요 수
     private List<String> pics;
 
     private FeedCommentGetRes comments; //댓글 정보
